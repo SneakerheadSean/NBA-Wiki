3dab7bc0717502415f4920154ebba612d2bbc5e1
laces <seanhughes23.sh@gmail.com>
2021-08-05 11:32:34 -0400

    Version 1.2
    Added an "About me" page as well as a navigation to other pages. Those pages still need to be built out but the structure is there.

--- app.js
+++ app.js
@@ -14,13 +14,10 @@ const nbaPlayers = async () => {
     playersData = await axios.get(playersImagesURL)
     const response = await axios.get(nbaPlayersURL)
     showData(response.data.data, playersData.data.league.standard)
-    // console.log(playersData.data.league.standard)
-        } catch (error) {
+  } catch (error) {
       console.error(error)
     }
 }
-
-// nbaPlayers()
 
 button.addEventListener('click', nbaPlayers)
 
@@ -32,8 +29,7 @@ function showData(players, idArray) {
   })
   console.log(currentPlayer)
   const playerImage = `http://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${currentPlayer.personId}.png`
-  // playerImages(playerImage)
-
+  
   players.forEach(player => {
     
     let playerData = `
@@ -42,8 +38,21 @@ function showData(players, idArray) {
     <h2>${player.team.full_name}</h2>
     <img src="${playerImage}"/>
     `
-    // imageResults.style.backgroundImage = `url(${playerImage})`
     results.insertAdjacentHTML('beforeend', playerData)
   });
 
+}
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "white";
 }
